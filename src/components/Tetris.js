c954680e2c2b95d3a827591cfff8b2b31066b623
Ruth Newman <ruthnewman@gmail.com>
2020-10-26 13:37:28 +0000

    added conditional rendering of game over

--- src/components/Tetris.js
+++ src/components/Tetris.js
@@ -13,18 +13,27 @@ import Display from './Display';
 import StartButton from './StartButton';
 
 const Tetris = () => {
-    console.log(createStage());
+    const [dropTime, setDropTime] = useState(null);
+    const [gameOver, setGameOver] = useState(false);
+
+    const [player] = usePlayer();
+    const [stage, setStage] = useStage(player);
+
+    console.log('re-render');
 
     return (
         <StyledTetrisWrapper>
             <StyledTetris>
-                <Stage stage={createStage()}/>
+                <Stage stage={stage}/>
                 <aside>
+                    {gameOver ? (
+                        <Display gameOver={gameOver} text="Game Over" />
+                    ) : (
                     <div>
                         <Display text="Score" />
                         <Display text="Rows" />
                         <Display text="Level" />
-                    </div>
+                    </div>)}
                     <StartButton />
                 </aside>
             </StyledTetris>
