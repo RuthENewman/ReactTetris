62b9fc49c79bb667b3055e846a0bb2e40be1e51b
Ruth Newman <ruthnewman@gmail.com>
2020-10-28 23:57:51 +0000

    additional logic for useStage

--- src/hooks/useStage.js
+++ src/hooks/useStage.js
@@ -1,17 +1,29 @@
 import { useState, useEffect } from 'react';
 import { createStage } from '../gameHelpers';
+import { usePlayer } from './usePlayer';
 
-export const useStage = () => {
+export const useStage = (player, resetTetromino) => {
     const [stage, setStage] = useState(createStage());
 
     useEffect(() => {
         const updateStage = prevStage => {
-            const newstage = prevStage.map(row => {
-                row.map(cell => ())
+            const newStage = prevStage.map(row => 
+                row.map(cell => (cell[1] === 'clear' ? [0, 'clear'] : cell)),
+            );
+            player.tetromino.forEach((row, y) => {
+                row.forEach((value, x) => {
+                    if (value !== 0) {
+                        newStage[y + player.position.y][x + player.position.x] = [
+                            value, 
+                            `${player.collided ? 'merged' : 'clear'}`,
+                        ]
+                    }
+                })
             })
+            return newStage;
         }
         setStage(prev => updateStage(prev));
-    }, [])
+    }, [player.collided, player.position.x, player.position.y, player.tetromino])
 
     return [stage, setStage];
 }
