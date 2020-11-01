785c50eb9684c3dd25f7b005d78a933d4ff60c87
Ruth Newman <ruthnewman@gmail.com>
2020-11-01 23:58:22 +0000

    clearing rows

--- src/hooks/useStage.js
+++ src/hooks/useStage.js
@@ -4,8 +4,22 @@ import { usePlayer } from './usePlayer';
 
 export const useStage = (player, resetPlayer) => {
     const [stage, setStage] = useState(createStage());
+    const [rowsCleared, setRowsCleared] = useState(0);
 
     useEffect(() => {
+        setRowsCleared(0);
+
+        const sweepRows = newStage => {
+            newStage.reduce((acc, row) => {
+                if (row.findIndex(cell => cell[0] === 0) === -1) {
+                    setRowsCleared((prev) => prev + 1);
+                    acc.unshift(new Array(newStage[0].length).fill(0, 'clear'));
+                    return acc;
+                }
+            });
+        }
+            
+
         const updateStage = prevStage => {
             const newStage = prevStage.map(row => 
                 row.map(cell => (cell[1] === 'clear' ? [0, 'clear'] : cell)),
